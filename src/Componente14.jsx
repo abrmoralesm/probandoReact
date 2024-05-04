a251f01e134c77e84885a7b7dad5f986a05917b8
abrmoralesm <abrmoralesm@gmail.com>
2024-05-04 13:47:10 +0200

    i

--- src/Componente14.jsx
+++ src/Componente14.jsx
@@ -1,3 +1,4 @@
+
  import React, { useState, useEffect } from "react";
 
 const Componente14 = ({ titulo }) => {
@@ -82,4 +83,5 @@ email:"",}
   );
 };
 export default Componente14;
+
 
--- src/Usarytirar.jsx
+++ src/Usarytirar.jsx
@@ -1,26 +1,30 @@
 import React, { useState } from "react";
+const Usarytirar = ({ titulo }) => {
+   const initialState = {
+     nombre: "",
+     apellidos: "",
+     email: "",
+   };
 
-const Usarytirar =({titulo})=>{
-
-  const initialState = {
-    nombre:"",
-    apellidos:"",
-    email:"",
-  };
-  const [formulario,setFormulario] =useState(initialState);
+  const [formulario, setFormulario]  = useState(initialState);
   const [mostrar,setMostrar] = useState(initialState);
-  const handleInput=(e)=>{
-    const {name,value}=e.target;
-    setFormulario(prevState=>({
-      ...prevState,
-      [name]:value
-    }))
-  }
-  const hacerClick=()=>{
-    setMostrar(formulario)
-  }
-  const {nombre,apellidos,email}=mostrar;
 
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setFormulario((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const mostrarClick = () => {
+    setMostrar(formulario);
+  };
+  const limpiarClick=()=>{
+    setFormulario(initialState)
+    setMostrar(initialState)
+  }
+  const { nombre, apellidos, email } = mostrar;
   return (
     <>
       <h1>{titulo}</h1>
@@ -43,20 +47,74 @@ const Usarytirar =({titulo})=>{
         </label>
         <label>
           Email:
-          <input
-          name="email"
-          value={formulario.email}
-          onChange={handleInput} />
+          <input name='email' value={formulario.email} onChange={handleInput} />
         </label>
-
       </form>
-      <button onClick={hacerClick}>Mostrar</button>
-      <br></br>
-      Nombre:{nombre} -  Apellidos:{apellidos} - Email:{email}
+      
+      <button onClick={mostrarClick}>Mostrar</button>
+      <button onClick={limpiarClick}>Limpiar</button>
+      <br/>
+      Nombre: {nombre} -  Apellidos:{apellidos}  - Email:{email}
     </>
   );
-}
+};
 export default Usarytirar;
+
+/* import React, { useState } from "react";
+
+const Usarytirar =({titulo})=>{
+  const initialState ={
+
+    nombre:"",
+    apellidos:"",
+    email:"",
+  }
+    const [formulario,setFormulario] = useState(initialState);
+    const [mostrar, setMostrar] = useState(initialState);
+
+    const handleInput =(e)=>{
+      const {name,value}= e.target
+      setFormulario(prevState=>({
+        ...prevState,
+        [name]:value
+      }))
+    }
+    const hacerClick=()=>{
+      setMostrar(formulario)
+    }
+
+    const {nombre, apellidos, email} = mostrar
+  return(
+    <>
+    <h1>{titulo}</h1>
+    <form>
+      <label>Nombre:
+        <input
+        name="nombre"
+        value={formulario.nombre}
+        onChange={handleInput} />
+      </label>
+      <label>Apellidos:
+        <input
+        name="apellidos"
+        value={formulario.apellidos}
+        onChange={handleInput}/>
+      </label>
+      <label>Email:
+        <input
+        name="email"
+        value={formulario.email}
+        onChange={handleInput} />
+      </label>
+    </form>
+    <button onClick={hacerClick}>Mostrar</button>
+    {nombre} - {apellidos} - {email}
+    </>
+  )
+}
+export default Usarytirar
+
+*/
 /* import React,{useState} from "react";
 
 const Usarytirar = ({titulo}) =>{
