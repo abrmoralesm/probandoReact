d0c804b95a26d9d637757b172a69649abc017f3c
abrmoralesm <abrmoralesm@gmail.com>
2024-05-30 11:04:27 +0200

    n

--- src/PruebaTecnica.jsx
+++ src/PruebaTecnica.jsx
@@ -1,5 +1,4 @@
 
-
 import React, { useState } from "react";
 
 const PruebaTecnica = ({ titulo }) => {
@@ -62,4 +61,7 @@ const PruebaTecnica = ({ titulo }) => {
 };
 
 export default PruebaTecnica;
+
+
+
 
--- src/Usarytirar.jsx
+++ src/Usarytirar.jsx
@@ -1,4 +1,56 @@
-import React, { useState } from "react";
+ import React, { useState } from "react";
+
+ const Usarytirar =({titulo})=>{
+  const initialState={
+    nombre:"",
+    apellidos:"",
+    email:"",
+  }
+  const [formulario,setFormulario] = useState(initialState)
+  const [mostrar,setMostrar]= useState(initialState)
+
+  const handleInput=(e)=>{
+    const {name,value}=e.target
+    setFormulario((prevState)=>({
+      ...prevState,
+      [name]:value
+    }))
+   }
+  const mostrarClick=()=>{
+    setMostrar(formulario)
+  }
+  const {nombre,apellidos,email}= mostrar
+  return(
+    <>
+    <h1>{titulo}</h1>
+    <form>
+      <label>Nombre:
+        <input
+        name="nombre"
+        value={formulario.nombre}
+        onChange={handleInput} />
+      </label>
+      <label>Apellidos:
+        <input
+        name="apellidos" 
+        value={formulario.apellidos}
+        onChange={handleInput}/>
+      </label>
+      <label>Email:
+      <input
+      name="email"
+      value={formulario.email}
+      onChange={handleInput} />
+      </label>
+    </form>
+    <button onClick={mostrarClick} >Mostrar</button>
+    Nombre: {nombre} Apellidos: {apellidos} Email: {email}
+    </>
+  )
+ }
+ export default Usarytirar
+ /*
+ import React, { useState } from "react";
 const Usarytirar = ({ titulo }) => {
    const initialState = {
      nombre: "",
@@ -58,120 +110,6 @@ const Usarytirar = ({ titulo }) => {
     </>
   );
 };
-export default Usarytirar;
-
-/* import React, { useState } from "react";
-
-const Usarytirar =({titulo})=>{
-  const initialState ={
-
-    nombre:"",
-    apellidos:"",
-    email:"",
-  }
-    const [formulario,setFormulario] = useState(initialState);
-    const [mostrar, setMostrar] = useState(initialState);
-
-    const handleInput =(e)=>{
-      const {name,value}= e.target
-      setFormulario(prevState=>({
-        ...prevState,
-        [name]:value
-      }))
-    }
-    const hacerClick=()=>{
-      setMostrar(formulario)
-    }
-
-    const {nombre, apellidos, email} = mostrar
-  return(
-    <>
-    <h1>{titulo}</h1>
-    <form>
-      <label>Nombre:
-        <input
-        name="nombre"
-        value={formulario.nombre}
-        onChange={handleInput} />
-      </label>
-      <label>Apellidos:
-        <input
-        name="apellidos"
-        value={formulario.apellidos}
-        onChange={handleInput}/>
-      </label>
-      <label>Email:
-        <input
-        name="email"
-        value={formulario.email}
-        onChange={handleInput} />
-      </label>
-    </form>
-    <button onClick={hacerClick}>Mostrar</button>
-    {nombre} - {apellidos} - {email}
-    </>
-  )
-}
-export default Usarytirar
-
-*/
-/* import React,{useState} from "react";
-
-const Usarytirar = ({titulo}) =>{
-    const initialState = {
-        nombre:"",
-        apellidos:"",
-        email:"",
-    }
-
-    const [formulario, setFormulario] = useState(initialState);
-    const [mostrar, setMostrar] = useState(initialState);
-
-    const handleInput =(e)=>{
-        const {name, value} = e.target;
-        setFormulario(prevState=>({
-          ...prevState,
-          [name]:value
-        }))
-    }
-
-    const hacerClick =()=>{
-        setMostrar(formulario);
-    }
-
-const { nombre, apellidos, email } = mostrar;
-    return (
-      <>
-        <h1>{titulo}</h1>
-        <label>
-          {" "}
-          Nombre:
-          <input
-            name='nombre'
-            value={formulario.nombre}
-            onChange={handleInput}
-          />
-        </label>
-        <label>Apellidos:
-            <input
-            name = 'apellidos'
-            value={formulario.apellidos}
-            onChange={handleInput} />
-            </label> 
-            <label>Email:
-                <input
-                name ="email"
-                value={formulario.email}
-                onChange={handleInput}
-                 />
-            </label>
-
-        <button onClick={hacerClick}>Mostrar</button>
-        <br />
-        {nombre} - {apellidos} - {email}
-      </>
-    );
-}
 export default Usarytirar;
 
 */
